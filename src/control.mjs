5cb8bf6979ec084b35df9a9ed86e78ec6245cc49
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-06-28 14:41:42 +0300

    static ui update

--- src/control.mjs
+++ src/control.mjs
@@ -1,3 +1,7 @@
+// import pkg from './proto/archer_protocol_pb.js';
+// const { HostPayload } = pkg;
+// console.log(new HostPayload())
+
 import pkg from './proto/archer_protocol_pb.js';
-const { HostPayload } = pkg;
-console.log(new HostPayload())
+const { TriggerCmd } = pkg;
+console.log(new pkg.TriggerCmd())
